a83b5511c65957ca573abef26205cf6bafd0e630
Katie Buchholz <kzbindy@hotmail.com>
2024-09-09 13:58:15 -0400

    created function to add row, but it's not showing up on the page; called the function and checked the names, but nothing's happening

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Note: each row is will contain an arrary: date, time, and blood sugar reading
+const currentRow = document.getElementById('row')
+
+// variable to find add reading button
+const addReading = document.getElementById('add_reading')
+
+
+// create function to add row each time event listener is being clicked
+
+function addRow() {
+    for (let i = 0; i < currentRow.length; i++) {
+        console.log(newRow)
+        // variables to create new row
+        const addNewRow = document.createElement('tr')
+        const dateData = document.createElement('td')
+        const timeData = document.createElement('td')
+        const readingData = document.createElement('td')
+        
+        addNewRow.append(dateData, timeData, readingData)
+        currentRow.append(addNewRow)
+    }
+}
+
+// event listener to make the button functional
+addReading.addEventListener('click', addRow())
